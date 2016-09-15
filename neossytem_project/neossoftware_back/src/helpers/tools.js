314e30f801b04e96d11af8431787dcbb181be269
hhugohm <hhugohm@gmail.com>
2016-09-14 22:08:38 -0500

    cambios back

---
+++ neossytem_project/neossoftware_back/src/helpers/tools.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+var toolHelper = {};
+
+
+toolHelper.sleep=function (milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+};
+
+module.exports = toolHelper;
